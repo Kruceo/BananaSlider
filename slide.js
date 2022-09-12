81c225abccbbf58441a9a3091c611ce7cb3997af
Kruceo <rafa2015krueger@gmail.com>
2022-09-11 23:27:39 -0300

    att: add new custom attributes

--- slide.js
+++ slide.js
@@ -1,16 +1,16 @@
 var threads = [];
-var animationSpeed = 1000;
-var cooldown = 5000;
+var animationSpeed = 750;
+var cooldown = 1000;
+var hover = 'stop'
+var animation = 'left'
 window.onload = function () {
-
     const sliderList = document.querySelectorAll("slider")
     if (sliderList.length > 0) {
         for (var slider of sliderList) {
             startSlider(slider)
         }
     }
-    else
-    {
+    else {
         console.warn('[banana-slider] No sliders here!\nIf you need help try check banana.kruceo.com\nCase you don\'t like this message, add banana.mess = false');
     }
 }
@@ -24,10 +24,10 @@ async function startSlider(yourSlider) {
     if (slider.getAttribute('speed')) {
         animationSpeed = slider.getAttribute('speed')
     }
+    if (slider.getAttribute('hover')) {
+        hover = slider.getAttribute('hover')
+    }
     slider.innerHTML = '<slider-frame>' + slider.innerHTML + '</slider-frame>';
-    //slider = slider.querySelector('slider');
-    //document.querySelector('slider');
-    //slider.outerHTML += "<div></div>";
     let frame = slider.querySelector('slider-frame');
     let itens = frame.querySelectorAll('slide');
     var index = 0;
@@ -37,19 +37,55 @@ async function startSlider(yourSlider) {
 
     slider.style.setProperty("overflow", "hidden");
     slider.style.setProperty("display", "block");
-    frame.style = "position: relative;left: 0%;display: grid;grid-auto-flow: column;grid-auto-columns: 1fr;--total-items:" + (itens.length) + ";width: calc(var(--total-items) * 100%);--index: 0;left: calc(var(--index)*-100%);transition: left " + animationSpeed + "ms;"
-    threads[threads.length] = setInterval(function () {
-        ;
-        frame.style.setProperty('--index', index);
-        index++;
+    switch (animation) {
+        case 'left':
+            frame.style = "position: relative;left: 0%;display: grid;grid-auto-flow: column;grid-auto-columns: 1fr;--total-items:" + (itens.length) + ";width: calc(var(--total-items) * 100%);--index: 0;--speed: " + animationSpeed + "ms;left: calc(var(--index)*-100%);transition: left var(--speed)";
+            break;
+        case 'right':
+            frame.style = "position: relative;left: 0%;display: grid;grid-auto-flow: column;grid-auto-columns: 1fr;--total-items:" + (itens.length) + ";width: calc(var(--total-items) * 100%);--index: 0;--speed: " + animationSpeed + "ms;left: calc(var(--index)*100%);transition: left var(--speed)";
+            break;
+        case 'up':
+            frame.style = "position: relative;top: 0%;--total-items:" + (itens.length) + ";height: calc(var(--total-items) * 100%);--index: 0;--speed: " + animationSpeed + "ms;top: calc(var(--index)*-100%);transition: top var(--speed)";
+            break;
 
-        if (index > max - 1) {
-            index = 0;
-        }
-    }, cooldown);
+        default:
+            break;
+    }
+    if (cooldown > 0) {
+        threads[threads.length] = setInterval(function () {
+            frame.style.setProperty('--index', index);
+            index++;
+            if (index > max - 1) {
+                index = 0;
+            }
+        }, cooldown);
+    }
+    switch (hover) {
+        case 'stop':
+            slider.addEventListener('mouseover', function (event) {
+                frame.style.setProperty('--speed', '100000s');
+            }, false)
+            slider.addEventListener('mouseout', function (event) {
+                frame.style.setProperty('--speed', animationSpeed + 'ms');
+            }, false)
+            break;
+        case 'add':
+            const thisIndex = frame.style.getPropertyValue('--index')
+
+            slider.addEventListener('mouseover', function (event) {
+                frame.style.setProperty('--index', thisIndex + 1);
+            }, true)
+            slider.addEventListener('mouseout', function (event) {
+                frame.style.setProperty('--index', thisIndex);
+            }, true)
+            break;
+
+        default:
+            break;
+    }
 }
 
-async function stopSlider() {
+async function stopAllSlider() {
     array.forEach(element => {
         clearInterval(element);
     });
