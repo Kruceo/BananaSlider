df572a11ad95f865341f810d3aacdf29991986a6
Kruceo <rafa2015krueger@gmail.com>
2023-03-20 15:52:47 -0300

    fix: unsificient slide width bug

--- dist/bundle.cjs.js
+++ dist/bundle.cjs.js
@@ -80,7 +80,7 @@ function startSlider(yourSlider) {
         initial +
         ";--speed: " +
         animationSpeed +
-        "ms;left: calc(var(--index)*-100%);transition: left var(--speed) " + animationCurve + ";height:100%;width:100%;";
+        "ms;left: calc(var(--index)*-100%);transition: left var(--speed) " + animationCurve + ";height:100%;width:"+itens.length*100+"%;";
       break;
     case "vertical":
       slider.style.setProperty("height", itens.sort((a, b) => a.offsetHeight - b.offsetHeight)[itens.length - 1].offsetHeight * 1 + 'px');
@@ -93,7 +93,7 @@ function startSlider(yourSlider) {
         ";--index: " + initial +
         ";--speed: " + animationSpeed +
         "ms;top: calc(var(--index)*-100%)" +
-      ";transition: top var(--speed)" + animationCurve + ";height:100%;width:100%;";
+      ";transition: top var(--speed)" + animationCurve + ";height:"+itens.length*100+";width:100%;";
       break;
   }
 
